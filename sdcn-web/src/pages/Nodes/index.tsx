50261cd06f9365a7875a0d2bcb25468fdfad4447
fiatrete <tomvpn2014@gmail.com>
2023-03-23 16:04:40 +0800

    prepare for portal deployment

--- sdcn-web/src/pages/Nodes/index.tsx
+++ sdcn-web/src/pages/Nodes/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import { Button } from 'antd'
 import cx from 'classnames'
-import { useNavigate } from 'react-router-dom'
 
 import styles from './index.module.css'
 
 const Nodes = () => {
-  const navigate = useNavigate()
-
   return (
     <div className={cx(styles.wrap)}>
-      <p>Nodes</p>
-      <Button
-        type='primary'
-        onClick={() => {
-          navigate('/')
-        }}
-      >
-        go to portal
-      </Button>
+      <p style={{ width: '100%', marginTop: '50px', textAlign: 'center' }}>
+        Coming soon :-)
+      </p>
     </div>
   )
 }
--- sdcn-web/src/pages/Playground/index.tsx
+++ sdcn-web/src/pages/Playground/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import { Button } from 'antd'
 import cx from 'classnames'
-import { useNavigate } from 'react-router-dom'
 
 import styles from './index.module.css'
 
 const Playground = () => {
-  const navigate = useNavigate()
-
   return (
     <div className={cx(styles.wrap)}>
-      <p>Playground</p>
-      <Button
-        type='primary'
-        onClick={() => {
-          navigate('/')
-        }}
-      >
-        go to portal
-      </Button>
+      <p style={{ width: '100%', marginTop: '50px', textAlign: 'center' }}>
+        Coming soon :-)
+      </p>
     </div>
   )
 }
