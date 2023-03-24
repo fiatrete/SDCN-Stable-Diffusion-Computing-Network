7209e50777d515c549a161055aa397d8f5c5663f
troy6en <troy6en@gmail.com>
2023-03-24 09:33:52 +0800

    fix(Playground): prettify Playbround page

--- sdcn-web/src/pages/Playground/index.tsx
+++ sdcn-web/src/pages/Playground/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Tabs } from 'antd';
-import type { TabsProps } from 'antd';
-import Txt2img from 'components/Txt2img';
+import React from 'react'
+import { Tabs } from 'antd'
+import type { TabsProps } from 'antd'
+import Txt2img from 'components/Txt2img'
 import cx from 'classnames'
 
-import styles from './index.module.css';
+import styles from './index.module.css'
 
 const items: TabsProps['items'] = [
   {
     key: '1',
     label: `txt2img`,
-    children: <Txt2img />
+    children: <Txt2img />,
   },
   {
     key: '2',
@@ -18,15 +18,14 @@ const items: TabsProps['items'] = [
     children: `img2img content`,
     disabled: true,
   },
-];
+]
 
 const Playground = () => {
   return (
     <div className={cx(styles.wrap)}>
-      <Tabs className={cx(styles.tabs)} defaultActiveKey="1" items={items} />
+      <Tabs className={cx(styles.tabs)} defaultActiveKey='1' items={items} />
     </div>
   )
 }
 
 export default Playground
-
