cbae40f3fb27d1624ba9c6da1fed84515778a337
synthintel <synthintel2@gmail.com>
2023-03-28 16:45:28 +0800

    feat(Playground): add Img2img impl to tabs

--- sdcn-web/src/pages/Playground/index.tsx
+++ sdcn-web/src/pages/Playground/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Tabs } from 'antd'
 import type { TabsProps } from 'antd'
 import Txt2img from 'components/Txt2img'
+import Img2img from 'components/Img2img'
 import cx from 'classnames'
 
 import styles from './index.module.css'
@@ -15,15 +16,14 @@ const items: TabsProps['items'] = [
   {
     key: '2',
     label: `img2img`,
-    children: `img2img content`,
-    disabled: true,
+    children: <Img2img />,
   },
 ]
 
 const Playground = () => {
   return (
     <div className={cx(styles.wrap)}>
-      <Tabs className={cx(styles.tabs)} defaultActiveKey='1' items={items} />
+      <Tabs className={cx(styles.tabs)} defaultActiveKey='2' items={items} />
     </div>
   )
 }
