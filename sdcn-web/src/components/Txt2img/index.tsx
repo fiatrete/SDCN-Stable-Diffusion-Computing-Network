44d3e0d90ef81deb8464371418fcd7521532da9c
synthintel <synthintel2@gmail.com>
2023-03-30 15:55:58 +0800

    refactor(txt2img): merge two form to one and wrap useCallback

--- sdcn-web/src/components/Txt2img/index.tsx
+++ sdcn-web/src/components/Txt2img/index.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import cx from 'classnames'
-import { Form, Select, Button, Input, Typography, FormInstance } from 'antd'
+import {
+  Form,
+  Select,
+  Button,
+  Input,
+  Typography,
+  FormInstance,
+  message,
+} from 'antd'
 import {
   ModelFormGroup,
   LoraFormGroup,
@@ -16,35 +24,27 @@ import styles from './index.module.css'
 const { Title } = Typography
 const { TextArea } = Input
 
-function InputAndGenerateArea({
-  form,
-  onButtonClicked,
-}: {
-  form: FormInstance<null>
-  onButtonClicked: () => void
-}) {
+function InputAndGenerateArea() {
   return (
     <div className={cx('flex flex-col items-start gap-6')}>
       <Title level={5}>Input keyword and generate</Title>
       <div className={cx('flex flex-col w-full items-start gap-6')}>
-        <Form form={form} name='txt2imgPromptForm' style={{ width: '100%' }}>
-          <Form.Item
-            name='prompt'
-            className={cx('self-stretch')}
-            style={{ marginBottom: '0px' }}
-          >
-            <TextArea
-              size='large'
-              rows={6}
-              placeholder='Enter prompts here'
-              className={cx('text-base leading-6 px-4 py-2')}
-            />
-          </Form.Item>
-        </Form>
-        <Button type='primary' onClick={onButtonClicked} size='large'>
-          Generate
-        </Button>
+        <Form.Item
+          name='prompt'
+          className={cx('self-stretch')}
+          style={{ marginBottom: '0px' }}
+        >
+          <TextArea
+            size='large'
+            rows={6}
+            placeholder='Enter prompts here'
+            className={cx('text-base leading-6 px-4 py-2')}
+          />
+        </Form.Item>
       </div>
+      <Button type='primary' htmlType='submit' size='large'>
+        Generate
+      </Button>
     </div>
   )
 }
@@ -55,11 +55,11 @@ const sizes = [
   { value: '768x512', label: '768x512' },
 ]
 
-function SettingsArea({ form }: { form: FormInstance<null> }) {
+function SettingsArea() {
   return (
     <div className={cx('flex flex-col w-80 gap-6 bg-green-000')}>
       <Title level={5}>Settings</Title>
-      <Form form={form} name='txt2imgSettingsForm' layout='vertical'>
+      <div className={cx('gap-0')}>
         <ModelFormGroup label='Model' name='model' />
 
         <Form.Item label='Size' name='size' initialValue={sizes[0].value}>
@@ -83,72 +83,62 @@ function SettingsArea({ form }: { form: FormInstance<null> }) {
           stepsName='steps'
           seedName='seed'
         />
-      </Form>
+      </div>
     </div>
   )
 }
 
 const Txt2img = () => {
-  const [SettingsForm] = Form.useForm()
-  const [PromptForm] = Form.useForm()
+  const [form] = Form.useForm()
 
   const [imgUri, setImgUri] = useState<string | undefined>()
   const [imgLoading, setImgLoading] = useState<boolean>(false)
 
-  const onGenerationButtonClicked = () => {
-    // First form submit
-    SettingsForm.submit()
-    // Second form submit
-    PromptForm.submit()
-  }
+  const onFormSubmit = useCallback(
+    async (name: string, { values }: FormFinishInfo) => {
+      try {
+        const [widthStr, heightStr] = values.size.split('x')
+        delete values.size
+        const apiParams: txt2imgParams = Object.assign(values)
+        apiParams.width = parseInt(widthStr)
+        apiParams.height = parseInt(heightStr)
+        apiParams.cfg_scale = 7
+        //console.log('submit txt2img', apiParams)
 
-  // For merging form data
-  let apiParams: txt2imgParams
-
-  const onFormSubmit = (name: string, { values }: FormFinishInfo) => {
-    if (name === 'txt2imgSettingsForm') {
-      // Received data in first submit, partial data
-      const [widthStr, heightStr] = values.size.split('x')
-      delete values.size
-      apiParams = Object.assign(values)
-      apiParams.width = parseInt(widthStr)
-      apiParams.height = parseInt(heightStr)
-      apiParams.cfg_scale = 7
-      console.log('first submit', apiParams)
-    }
-    if (name === 'txt2imgPromptForm') {
-      // Received data in second submit, full data
-      apiParams = Object.assign(apiParams, values)
-      console.log('second submit, merged', apiParams)
-      ;(async () => {
         setImgLoading(true)
         setImgUri(await txt2img(apiParams))
+      } catch (err) {
+        if (err instanceof String) message.error(err)
+        if (err instanceof Error) message.error(err.message)
+      } finally {
         setImgLoading(false)
-      })()
-    }
-  }
+      }
+    },
+    [],
+  )
 
   return (
     /* when Form submitted, the parent Form.Provider received the submittion via onFormFinish */
     <Form.Provider onFormFinish={onFormSubmit}>
-      <GeneratingMask open={imgLoading} />
-      <div
-        className={cx(
-          styles.wrap,
-          'flex flex-col md:flex-row w-full bg-yellow-000 gap-24 mt-8',
-        )}
-      >
-        <div className={cx('flex flex-col flex-1 bg-red-000')}>
-          <InputAndGenerateArea
-            onButtonClicked={onGenerationButtonClicked}
-            form={PromptForm}
-          />
-          <div style={{ height: '788px' }}>
-            <ImageWidget src={imgUri} />
+      <Form name='txt2imgForm' form={form} layout='vertical'>
+        <GeneratingMask open={imgLoading} />
+        <div
+          className={cx(
+            styles.wrap,
+            'w-full flex flex-col md:flex-row bg-yellow-000 gap-24 mt-8',
+          )}
+        >
+          <div className={cx('w-full flex flex-col flex-1 bg-red-000')}>
+            <InputAndGenerateArea />
+            <div className={cx('h-[788px] w-full flex justify-center')}>
+              <div className={cx('w-[600px] flex')}>
+                <ImageWidget src={imgUri} />
+              </div>
+            </div>
           </div>
+          <SettingsArea />
         </div>
-        <SettingsArea form={SettingsForm} />
-      </div>
+      </Form>
     </Form.Provider>
   )
 }
