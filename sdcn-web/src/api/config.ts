c584776942aaa6bcfdd61e37a390113cbd73c861
fiatrete <fiatrete@gmail.com>
2023-04-04 09:46:40 +0800

    set baseApiUrl

--- sdcn-web/src/api/config.ts
+++ sdcn-web/src/api/config.ts
@@ -1,5 +1,6 @@
 class ApiConfig {
-  private baseApiUrl = 'http://localhost:9080'
+  // private baseApiUrl = 'http://localhost:9080'
+  private baseApiUrl = 'https://api.sdcn.info'
 
   private successCode = 200
 
